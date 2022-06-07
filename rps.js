6967995d9d75b9c6687bfd93d15525c262ae6b84
Frizzybman11 <Frizzybman11@aol.com>
2022-06-07 17:21:20 -0400

    Create playRound function
    Examines the player's + computer's selections and determines a winner using a switch statement

--- rps.js
+++ rps.js
@@ -1,7 +1,34 @@
-function randomNum() {
-    let randomNum = Math.random()*100;
-    let num = randomNum.toFixed(0);
-    return num
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection, computerSelection) {
+        case ("Rock", "Rock"):
+            roundMessage = "Draw! Try again!";
+            break;
+        case ("Rock", "Paper"):
+            roundMessage = "You lose! Paper beats Rock!";
+            break;
+        case ("Rock", "Scissors"):
+            roundMessage = "You win! Rock beats Scissors!";
+            break;
+        case ("Paper", "Paper"):
+            roundMessage = "Draw! Try again!";
+            break;
+        case ("Paper", "Rock"):
+            roundMessage = "You win! Paper beats Rock!";
+            break;
+        case ("Paper", "Scissors"):
+            roundMessage = "You lose! Scissors beats Paper!";
+            break;
+        case ("Scissors", "Scissors"):
+                roundMessage = "Draw! Try again!";
+                break;
+        case ("Scissors", "Rock"):
+            roundMessage = "You lose! Rock beats Scissors!";
+            break;
+        case ("Scissors", "Paper"):
+            roundMessage = "You win! Scissors beats Paper!";
+            break;
+    }
+    return roundMessage
 }
 
 function computerPlay() {
