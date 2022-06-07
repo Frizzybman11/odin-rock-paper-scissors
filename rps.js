9a4ef2ebced7ee7fec057dd5cf7683ba0d71173d
Frizzybman11 <Frizzybman11@aol.com>
2022-06-07 16:54:52 -0400

    Create computerPlay function
    Computer will randomly select "Rock," "Paper," or "Scissors" based on random number.

--- rps.js
+++ rps.js
@@ -1,3 +1,17 @@
-<script>
-    
-</script>
+function randomNum() {
+    let randomNum = Math.random()*100;
+    let num = randomNum.toFixed(0);
+    return num
+}
+
+function computerPlay() {
+    let randomNum = Math.random()*100;
+    let num = randomNum.toFixed(0);
+    if (num < 33) {
+        return "Rock"
+    } else if (num < 66) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
